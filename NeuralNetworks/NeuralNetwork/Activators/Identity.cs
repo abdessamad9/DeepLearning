912fc6b071c013dbf13721a780fbee76ece1468d
abdessamad9 <39315497+abdessamad9@users.noreply.github.com>
2020-01-23 11:34:23 +0100

    modification de la structure

---
+++ NeuralNetworks/NeuralNetwork/Activators/Identity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork.Activators
+{
+    class Identity : IActivator
+    {
+        public Identity()
+        {
+
+        }
+        public Func<double, double> Apply => d => d;
+        public Func<double, double> ApplyDerivative => d => 1;
+
+        public ActivatorType Type => ActivatorType.Identity;
+    }
+}
---
+++ NeuralNetworks/NeuralNetwork/Activators/Sigmoid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork.Activators
+{
+    class Sigmoid : IActivator
+    {
+        public Sigmoid()
+        {
+
+        }
+        public Func<double, double> Apply => d => 1.0/(1.0+Math.Exp(-d));
+        public Func<double, double> ApplyDerivative => d => 1.0 / (1.0 + Math.Exp(-d)) * (1.0- 1.0 / (1.0 + Math.Exp(-d))) ;
+
+        public ActivatorType Type => ActivatorType.Sigmoid;
+    }
+    
+}
