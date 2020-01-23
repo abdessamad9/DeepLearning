f81129b053078e6a82581e33e2c4f6b52c804516
Diocoura <48685594+Diocoura@users.noreply.github.com>
2020-01-23 12:12:40 +0100

    Application

---
+++ Application/Application/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+//using NeuralNetwork.Common;
+
+namespace Application
+{
+    public class Resaux
+    {
+
+        int main()
+        {
+            /* std::cout << "Hello World!\n";
+
+             var serialized = NetworkSerializer.Serialize(network);
+             JsonSerializer serializer = new JsonSerializer();
+             var filename = "/path/to/my-network.json";
+             using (StreamWriter sw = new StreamWriter(filename))
+             using (JsonWriter writer = new JsonTextWriter(sw))
+             {
+                 serializer.Serialize(writer, serialized);
+             }*/
+            return 0;
+
+        }
+
+    }
+}
