260e3a7652d55150c8343e3a6ac6c526507f78f2
Diocoura <48685594+Diocoura@users.noreply.github.com>
2020-01-30 11:41:23 +0100

    Program

--- NeuralNetworks/Test/Program.cs
+++ NeuralNetworks/Test/Program.cs
@@ -9,7 +9,6 @@ using NeuralNetwork.Activators;
 using NeuralNetwork.Layers;
 using NeuralNetwork.Common.Activators;
 
-using NeuralNetwork.Common.Activators;
 
 using NeuralNetwork.Serialization;
 using Newtonsoft.Json;
@@ -36,10 +35,10 @@ namespace TestPricing
              network.Layers[2] = new Standard(batchSize, activator, new double[256], new double[256, 256], new FixedLearningRateParameters(0.1));
              network.Layers[3] = new Standard(batchSize, activator, new double[1], new double[1, 1], new FixedLearningRateParameters(0.1));*/
 
-            network.Layers[0] = new Standard(batchSize, activator, new double[200], new double[7, 200], new FixedLearningRateParameters(0.1));
-            network.Layers[1] = new Standard(batchSize, activator, new double[130], new double[200, 130], new FixedLearningRateParameters(0.1));
-            network.Layers[2] = new Standard(batchSize, activator, new double[50], new double[130, 50], new FixedLearningRateParameters(0.1));
-            network.Layers[3] = new Standard(batchSize, activator, new double[1], new double[50, 1], new FixedLearningRateParameters(0.1));
+            network.Layers[0] = new Standard(batchSize, activator, new double[200], new double[7, 200], new FixedLearningRateParameters(0.1),1);
+            network.Layers[1] = new Standard(batchSize, activator, new double[130], new double[200, 130], new FixedLearningRateParameters(0.1),2);
+            network.Layers[2] = new Standard(batchSize, activator, new double[50], new double[130, 50], new FixedLearningRateParameters(0.1),3);
+            network.Layers[3] = new Standard(batchSize, activator, new double[1], new double[50, 1], new FixedLearningRateParameters(0.1),4);
 
             var serialized = NetworkSerializer.Serialize(network);
              JsonSerializer serializer = new JsonSerializer();
